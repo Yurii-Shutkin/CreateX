4026499104381a769e08b33aea5cfd65c286ce40
Yurii-Shutkin <yurashutkinn@gmail.com>
2025-12-24 02:06:26 +0200

    finished projects block adaptive layout on homepage

---
+++ build/js/homepage/projectsSwiper.js
@@ -0,0 +1,36 @@
+const projectSwiper = new Swiper('.projects__swiper', {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  loop: true,
+
+  navigation: {
+    nextEl: '.btn-next',
+    prevEl: '.btn-prev',
+  },
+
+  breakpoints: {
+    425: {
+      slidesPerView: 1,
+      slidesPerGroup: 1
+    },
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 1
+    },
+    1024: {
+      slidesPerView: 2.6,
+      slidesPerGroup: 1
+    },
+
+    1920: {
+      slidesPerView: 3,
+      slidesPerGroup: 1
+    },
+  },
+ 
+
+  observer: true,              
+  observeSlideChildren: true,
+});
+
+
