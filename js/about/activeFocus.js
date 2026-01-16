9b578242797b247920ae60dd13537196a15c16ee
Yurii-Shutkin <yurashutkinn@gmail.com>
2026-01-16 18:05:29 +0200

    Update 2026-01-16T16:05:19.746Z

--- js/about/activeFocus.js
+++ js/about/activeFocus.js
@@ -1,8 +1,8 @@
 const activeElement = document.querySelector('.swiper-slide-thumb-active');
 if (activeElement) {
   activeElement.scrollIntoView({
-    behavior: 'smooth', // Плавная прокрутка
-    block: 'nearest',   // Прокрутить минимально, чтобы элемент стал видимым
+    behavior: 'smooth',
+    block: 'nearest',   
     inline: 'nearest'
   });
 }
