7657d8d4b415ef2c0be77b74cdb440b87b2aef40
Yurii-Shutkin <yurashutkinn@gmail.com>
2026-01-16 12:56:51 +0200

    deploy: github pages

--- src/js/about/activeFocus.js
+++
@@ -1,8 +0,0 @@
-const activeElement = document.querySelector('.swiper-slide-thumb-active');
-if (activeElement) {
-  activeElement.scrollIntoView({
-    behavior: 'smooth', // Плавная прокрутка
-    block: 'nearest',   // Прокрутить минимально, чтобы элемент стал видимым
-    inline: 'nearest'
-  });
-}
--- src/js/components/footer.js
+++
@@ -1,9 +0,0 @@
-const scrollToTopButton = document.querySelector('.to-up-btn');
-
-scrollToTopButton.addEventListener('click', function() {
-  window.scroll({
-    top: 0,
-    behavior: 'smooth' 
-  });
-  console.log('click')
-});
